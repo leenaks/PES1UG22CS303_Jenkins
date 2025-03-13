4a04797b8a234474b16e3d83fed5727c276d1c55
leenaks <138242408+leenaks@users.noreply.github.com>
2025-03-13 16:01:10 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 int main(void) {
   cout << "Hello, World\n";
-  cout << "Hello, Jenkins\n";
-  cout << "I have successfully built and run this  \n";
+  cout << "Hello, Leena\n";
+  cout << "I have successfully built , run this  \n";
   return 0;
 }
